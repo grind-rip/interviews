55c477e68167a7f94438680acea0af310b0e30e3
nickolashkraus <0x@nickolaskraus.io>
2025-01-17 10:25:17 -0600

    Small fix

--- amazon/demo.go
+++ amazon/demo.go
@@ -6,10 +6,10 @@ import "fmt"
 //
 // The function accepts INTEGER n as parameter.
 
-// FizzBuzz prints "Fizz" instead of the number for each multiple of 3, prints
+// fizzBuzz prints "Fizz" instead of the number for each multiple of 3, prints
 // "Buzz" instead of the number for each multiple of 5, and prints "FizzBuzz"
 // instead of the number for numbers which are multiples of both 3 and 5.
-func FizzBuzz(n int32) {
+func fizzBuzz(n int32) {
 	// Constraints:
 	//   * 0 < n < 2 * 10^5
 	if n <= 0 || n >= 200000 {
--- amazon/demo_test.go
+++ amazon/demo_test.go
@@ -53,7 +53,7 @@ func TestFizzBuzz(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			output := captureOutput(func() {
-				FizzBuzz(tt.input)
+				fizzBuzz(tt.input)
 			})
 
 			if output != tt.expected {
